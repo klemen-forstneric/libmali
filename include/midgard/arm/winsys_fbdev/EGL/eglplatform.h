5cbe74991d458c8e19ef80aeb18ea8c46fbfa76e
Klemen Forstnerič <klemen-forstneric@users.noreply.github.com>
2019-04-05 14:15:56 +0200

    Update eglplatform.h

--- include/midgard/arm/winsys_fbdev/EGL/eglplatform.h
+++ include/midgard/arm/winsys_fbdev/EGL/eglplatform.h
@@ -1,65 +1,54 @@
 /*
  * This confidential and proprietary software may be used only as
- * authorised by a licensing agreement from Rockchip.
- * Copyright (c) 2017 FUZHOU ROCKCHIP ELECTRONICS CO., LTD. ("Rockchip").
- * 	http://www.rock-chips.com
- * 		ALL RIGHTS RESERVED
+ * authorised by a licensing agreement from ARM Limited
+ * (C) COPYRIGHT 2008-2010, 2013 ARM Limited
+ * ALL RIGHTS RESERVED
  * The entire notice above must be reproduced on all authorised
  * copies and copies may only be made to the extent permitted
- * by a licensing agreement from Rockchip.
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * by a licensing agreement from ARM Limited.
  */
 
-/**
- * @file eglplatform.h
- * Platform related definitions
- */
-
-#ifndef _EGLPLATFORM_H_
-#define _EGLPLATFORM_H_
+#ifndef __EGLPLATFORM_H__
+#define __EGLPLATFORM_H__
 
 #ifdef __cplusplus
-extern "C"
-{
-#endif /* __cplusplus */
-
-
-#include <KHR/khrplatform.h>
-#include <stdint.h>
-
-#include "mali_fbdev_types.h"
-
-#ifndef EGLAPI
-#define EGLAPI
+extern "C" {
 #endif
 
 #ifndef EGLAPIENTRY
 #define EGLAPIENTRY
 #endif
-
 #ifndef EGLAPIENTRYP
-#define EGLAPIENTRYP EGLAPIENTRY *
+#define EGLAPIENTRYP EGLAPIENTRY*
+#endif
+#ifndef EGLAPI
+#define EGLAPI extern
 #endif
 
-typedef fbdev_window * EGLNativeWindowType;
+#include <EGL/fbdev_window.h>
+#include <KHR/khrplatform.h>
 
-typedef int32_t EGLNativePixmapType;
-
-typedef void * EGLNativeDisplayType;
-
-typedef EGLNativeWindowType NativeWindowType;
-typedef EGLNativePixmapType NativePixmapType;
-typedef EGLNativeDisplayType NativeDisplayType;
-
-typedef int32_t EGLint;
+typedef fbdev_window *NativeWindowType;
+typedef void *NativePixmapType;
+typedef void *NativeDisplayType;
 
 #ifdef __cplusplus
 }
-#endif /* __cplusplus */
+#endif
 
-#endif /* _EGLPLATFORM_H_ */
+/* EGL 1.2 types, renamed for consistency in EGL 1.3 */
+typedef NativeDisplayType EGLNativeDisplayType;
+typedef NativePixmapType EGLNativePixmapType;
+typedef NativeWindowType EGLNativeWindowType;
+
+/* Define EGLint. This must be an integral type large enough to contain
+ * all legal attribute names and values passed into and out of EGL,
+ * whether their type is boolean, bitmask, enumerant (symbolic
+ * constant), integer, handle, or other.
+ * While in general a 32-bit integer will suffice, if handles are
+ * represented as pointers, then EGLint should be defined as a 64-bit
+ * integer type.
+ */
+typedef int EGLint;
+
+#endif /* __EGLPLATFORM_H__ */
